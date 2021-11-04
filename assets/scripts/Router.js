a621400bda67f31237a3ec3809075e0200501571
Yekaterina Ruslanovna Kostina <yekostina@gmail.com>
2021-11-03 20:56:14 -0700

    update lab

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -1,16 +1,16 @@
 // router.js
 
 /** Some hints for this router:
-  *   - it shouldn't be a terribly long file, each function is pretty short.
-  *   - the functions being passed in should mostly be stored so that
-  *     you can call them later when you want to navigate to a page
-  *   - you should be pushing to history (only when the 'popstate' event
-  *     hasn't fired) so that you can use forward / backward buttons
-  *   - You should be using hashes to update the URL (e.g. 
-  *     https://somewebsite.com#somePage) - the hash is the #somePage part.
-  *     It's accessible via window.location.hash and using them lets you
-  *     easily modify the URL without refreshing the page or anything
-  */
+ *   - it shouldn't be a terribly long file, each function is pretty short.
+ *   - the functions being passed in should mostly be stored so that
+ *     you can call them later when you want to navigate to a page
+ *   - you should be pushing to history (only when the 'popstate' event
+ *     hasn't fired) so that you can use forward / backward buttons
+ *   - You should be using hashes to update the URL (e.g.
+ *     https://somewebsite.com#somePage) - the hash is the #somePage part.
+ *     It's accessible via window.location.hash and using them lets you
+ *     easily modify the URL without refreshing the page or anything
+ */
 
 export class Router {
   /**
@@ -24,6 +24,7 @@ export class Router {
      * TODO Part 1
      * Fill in this function as specified in the comment above
      */
+    this.home = homeFunc;
   }
 
   /**
@@ -38,6 +39,8 @@ export class Router {
      * TODO Part 1
      * Fill in this function as specified in the comment above
      */
+    this.page = page;
+    this.pageFunction = pageFunc;
   }
 
   /**
